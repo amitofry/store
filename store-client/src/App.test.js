d38c11a2d72e8d1f1ef16a469655bfedcbac84a1
amit <amit.ofry@gmail.com>
2019-08-28 23:18:57 +0300

    added tests

--- store-client/src/App.test.js
+++ store-client/src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {withRouter} from 'react-router'
+import {Link, Route, Router, Switch} from 'react-router-dom'
+import {createMemoryHistory} from 'history'
+import {render, fireEvent} from '@testing-library/react'
 import App from './App';
 
-it('renders without crashing', () => {
+function renderApp () {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  return ReactDOM.render(<App />, div);
+}
+
+function renderWithRouter(
+  ui,
+  {route = '/', history = createMemoryHistory({initialEntries: [route]})} = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  }
+}
+
+it('main page not logged in', () => {
+  renderApp()
 });
+
+test('main page logged in', () => {
+  Object.defineProperty(document, 'cookie', {
+    get: jest.fn().mockImplementation(() => { return "cookie={%22userName%22:%22amit%22%2C%22isLoggedIn%22:true}"; }),
+    set: jest.fn().mockImplementation(() => {}),
+  });
+  
+  const {container} = renderWithRouter(<App />)
+  expect(container.querySelector('h1').innerHTML).toMatch('Stop Dreaming Start LIVING!')
+})
--- store-client/src/components/ProductComponent/Product.js
+++ store-client/src/components/ProductComponent/Product.js
@@ -15,7 +15,7 @@ export default class Product extends Component {
 
     render() {
       return (
-        <div className="card product" style={{width:'18rem'}}>
+        <div className="card product" style={{width:'18rem'}} data-testid="product">
             <img src={this.props.imageSrc} className="card-img-top" alt={this.props.name}/>
             <div className="card-body">
             <h5 className="card-title">{this.props.name}</h5>
